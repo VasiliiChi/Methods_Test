b20290b3d847eb9b6d725f3997ed4f49e57ec8c0
VasiliiChibisov <chibisowasya@gmail.com>
2022-03-26 16:01:39 +0300

    Добавили в метод ShowColor () входные параметры

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,9 +4,9 @@ namespace Methods
 {
     class Program
     {
-        static string ShowColor()
+        static string ShowColor(string username, int userage)
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            Console.WriteLine("{0}, {1}. \nНапишите свой любимый цвет на английском с маленькой буквы.", username, userage);
             var color = Console.ReadLine();
 
             switch (color)
@@ -41,12 +41,21 @@ namespace Methods
         }
         static void Main(string[] args)
         {
+            (string name, int age) anketa;
+
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
             Console.WriteLine("Цикл for");
 
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine(i);
-                ShowColor();
+                ShowColor(anketa.name, anketa.age);
             }
 
             Console.WriteLine("Цикл while");
@@ -54,7 +63,7 @@ namespace Methods
             while (k < 3)
             {
                 Console.WriteLine(k);
-                ShowColor();
+                ShowColor(anketa.name, anketa.age);
                 k++;
             }
 
@@ -65,15 +74,15 @@ namespace Methods
             {
                 Console.WriteLine(t);
                 t++;
-                ShowColor();
+                ShowColor(anketa.name, anketa.age);
             } while (t < 3);
 
+           
 
-            
             var favcolors = new string[3];
             for (int i = 0; i < favcolors.Length; i++)
             {
-                favcolors[i] = ShowColor();
+                favcolors[i] = ShowColor(anketa.name, anketa.age);
             }
             Console.WriteLine("Ваши любимые цвета: ");
             foreach (var colors in favcolors)
