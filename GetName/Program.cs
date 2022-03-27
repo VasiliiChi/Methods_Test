a4ee1f538db15bfe47f5465967d5686ec72f6d0e
VasiliiChibisov <chibisowasya@gmail.com>
2022-03-27 20:07:53 +0300

    Передача метода по значению

--- GetName/Program.cs
+++ GetName/Program.cs
@@ -6,9 +6,29 @@ namespace GetName
     {
         static void Main(string[] args)
         {
+            var someName = "Евгения";
+            Console.WriteLine(someName);
+            var someAge = 22;
+            Console.WriteLine(someAge);
 
+            GetName(someName);
+            Console.WriteLine(someName);
+
+            GetAge(someAge);
+            Console.WriteLine(someAge);
 
             Console.ReadKey();
+        }
+        static void GetName(string name)
+        {
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+        }
+
+        static void GetAge(int age)
+        {
+            Console.WriteLine("Введите возраст");
+            age = int.Parse(Console.ReadLine());
         }
     }
 }
